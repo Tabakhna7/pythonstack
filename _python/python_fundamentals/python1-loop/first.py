a9a794ceb5760d71e0adc5c20443115f56212014
Tabakhna7 <Tabakhna7@gmail.com>
2024-04-28 15:20:47 +0300

    add for loop assignment

---
+++ _python/python_fundamentals/python1-loop/first.py
@@ -0,0 +1,38 @@
+for x in range(151):
+    print(x)
+
+
+for i in range(5,1001,5):
+    print(i)
+
+
+for y in range(1,101,1):
+    if y%10==0 :
+        print( "Coding Dojo")
+
+    elif y%5==0 :
+        print("Coding")
+
+    else :
+        print(y)
+
+
+sum=0
+for t in range(0,5000001,1):
+    if t%2!=0:
+        
+
+        sum+=t
+print(sum)
+
+for k in range(2018,0,-4):
+    print(k)
+
+
+LowNum=2
+highNum=9
+mult=3
+
+for j in range(LowNum,highNum+1):
+  if j%mult==0: 
+    print(j)
