272215b664e2cd52ea3cc356605d23355b1ac658
Tabakhna7 <Tabakhna7@gmail.com>
2024-05-06 13:21:20 +0300

    Added html table

--- flask/flask_fundamentals/playground/play.py
+++ flask/flask_fundamentals/playground/play.py
@@ -3,13 +3,12 @@ app = Flask(__name__)
 @app.route('/play')
 def render_three_blue_boxes():
     return render_template('index.html', num_boxes=3, color='blue')
-@app.route('/play/<int:x>')
+@app.route('/play/<x>')
 def render_boxes(x):
-    return render_template('index.html', num_boxes=x, color='blue')
-@app.route('/play/<int:x>/<color>')
+    return render_template('index.html', num_boxes=int(x), color='blue')
+@app.route('/play/<x>/<color>')
 def render_colored_boxes(x, color):
-    return render_template('index.html', num_boxes=x, color=color)
-
+    return render_template('index.html', num_boxes=int(x), color=color)
 
 if __name__=="__main__":    
     app.run(debug=True)   
