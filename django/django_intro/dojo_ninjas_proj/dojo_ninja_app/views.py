68f82e1fab62412f3064d4232d7a5c0fe133a2da
Tabakhna7 <Tabakhna7@gmail.com>
2024-05-15 15:05:10 +0300

    added Dojo_ninja_template

---
+++ django/django_intro/dojo_ninjas_proj/dojo_ninja_app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,redirect
+from .models import Dojo,Ninja
+def index(request):
+    data = {
+        'dojos':Dojo.objects.all()
+            }
+       
+
+    
+    return render(request, "index.html" ,data)
+
+def add_dojo(request):
+    Dojo.objects.create(name=request.POST["name"],
+                        city=request.POST["city"],
+                        state=request.POST["state"]
+                        )
+    return redirect('/')
+
+def add_ninja(request):
+    selected=Dojo.objects.get(id=request.POST["dojo_id"])
+    Ninja.objects.create(
+        first_name=request.POST["first_name"],
+        last_name=request.POST["last_name"],
+        dojo=selected
+         
+         
+    )
+    
+    return redirect('/')
